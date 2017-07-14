b135717402130aec30e1fc6841865c4ff987eae3
Tony Storti <tonystorti@gmail.com>
2017-07-14 14:45:04 +0000

    added moment and BME sensor packages

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 
 var keys = require("./keys.js");
 var firebase = require('firebase');
-
+var BME280 = require('node-adafruit-bme280');
+var moment = require("moment");
 
 firebase.initializeApp(keys.firebaseKeys);
 
-//set basic variables for new child in firebase
-firebase.database().ref().child("test").set({
-	"name": "test record"
+var currentTemp=null;
+var currentHumidity=null;
+var currentPressure=null;
+console.log(moment().format("LTS"));
+
+BME280.probe(function(temperature, pressure, humidity) {
+	//temperature in C
+	currentTemp = temperature;
+	//pressure in pascals
+	currentPressure = pressure;
+	//percentage humidity
+	currentHumidity = humidity;
+	firebase.database().ref().child("test").set({
+		"date": moment().format("L"),
+		"time":moment().format("LTS"),
+		"temperature": currentTemp,
+		"humidity": currentHumidity,
+		"pressure": currentPressure,
+	});
+  
 });
+
